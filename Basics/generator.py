394e09be4f9c361025d0034832b28b847e81e33c
najnin26 <najninshirin26@gmail.com>
2024-03-20 21:43:39 +0600

    Update generator.py

--- Basics/generator.py
+++ Basics/generator.py
@@ -1,10 +1,15 @@
 def my_func():
-    yield "a"
-    yield "b"
-    yield "c"
+    #yield "a"
+    #yield "b"
+    #yield "c"
+    for i in range(5):
+        print("---------",i)
+        yield i
     
 x=my_func()
 print(next(x))
 print(next(x))
 print(next(x))
 print(next(x))
+print(next(x))
+
