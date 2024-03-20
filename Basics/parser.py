68870465d49d78dfd7bf15eeb210c004fcac1102
najnin26 <najninshirin26@gmail.com>
2024-03-20 22:52:00 +0600

    Update parser.py

--- Basics/parser.py
+++ Basics/parser.py
@@ -4,4 +4,10 @@ if __name__ == '__main__':
     parser=argparse.ArgumentParser(
         description="my watch script"
         )
+    
+    parser.add_argument('num1', help="Number 1")
+    parser.add_argument('num2', help="Number 2")
+    parser.add_argument('operation', help="provide operator")
+    
     args=parser.parse_args()
+    print(args)
