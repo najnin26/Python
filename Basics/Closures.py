4a7fb470018aab8775515ec5688f381e0f97b5fd
najnin26 <najninshirin26@gmail.com>
2024-03-21 21:39:15 +0600

    Update Closures.py

--- Basics/Closures.py
+++ Basics/Closures.py
@@ -1,7 +1,18 @@
-def OuterFunction(text):
-    def InnerFunction():
-        print(text)
-    InnerFunction()
+# def OuterFunction(text):
+#     def InnerFunction():
+#         print(text)
+#     InnerFunction()
     
-OuterFunction("Hello")
+# OuterFunction("Hello")
 
+def pop(list):
+    def get_last_item(my_list):
+        return my_list[len(list)-1]
+    
+    list.remove(get_last_item(list))
+    return list
+
+a=[1,2,3,4,6]
+print(pop(a))
+print(pop(a))
+print(pop(a))
