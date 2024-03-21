deba008a2ff2361df8709925255516eaf1549d9d
najnin26 <najninshirin26@gmail.com>
2024-03-21 21:52:42 +0600

    Update Closures.py

--- Basics/Closures.py
+++ Basics/Closures.py
@@ -1,3 +1,6 @@
+#NESTED FUNCTION
+
+
 # def OuterFunction(text):
 #     def InnerFunction():
 #         print(text)
@@ -16,3 +19,31 @@ a=[1,2,3,4,6]
 print(pop(a))
 print(pop(a))
 print(pop(a))
+
+#CLOSURE
+
+# def OuterFunction(text):
+#     def InnerFunction():
+#         print(text)
+#     return InnerFunction
+    
+# a=OuterFunction("Hello")
+# del OuterFunction
+# a()
+
+def nth_power(exponent):
+    def pow_of(base):
+        return pow(base,exponent)
+    return pow_of
+
+s=nth_power(2)
+print(s(2))
+print(s(3))
+print(s(4))
+print(s(5))
+
+cube=nth_power(3)
+print(cube(2))
+print(cube(3))
+print(cube(4))
+print(cube(5))
