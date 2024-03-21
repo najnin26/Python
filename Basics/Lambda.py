c05477908b329969be621cc80a29c0ddd7d393d3
najnin26 <najninshirin26@gmail.com>
2024-03-21 20:06:34 +0600

    Update Lambda.py

--- Basics/Lambda.py
+++ Basics/Lambda.py
@@ -12,3 +12,12 @@ product=lambda x,y,z: x*y*z
 print(double(10))
 print(add(10,20))
 print(product(10,20,30))
+
+#map
+my_list=[2,5,8,10,9,3]
+my_list2=[1,4,7,8,5,1]
+
+a=map(lambda x: x*2, my_list)
+print(list(a))
+b=map(lambda x,y: x+y,my_list, my_list2)
+print(list(b))
